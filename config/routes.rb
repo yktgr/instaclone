65924894d96ede1635fc5916c0ea06ffc88fa8aa
user <user@usernoMacBook-Pro.local>
2019-06-19 21:34:06 +0900

    セッション→ポスト作成途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources:users,only:[:new,:create,:destroy,:show]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources:posts
+  resources:sessions,only:[:new,:create,:destroy]
+  resources:users
 end
