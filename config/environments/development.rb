63048ea5ab54d748708820d41dee8cdd102f8f5d
user <user@usernoMacBook-Pro.local>
2019-07-02 22:44:42 +0900

    actionmailer再設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
+
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'kinder-worms-64576.herokuapp.com' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "in_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'kinder-worms-64576.herokuapp.com' }
+  config.action_mailer.delivery_method = :letter_opener_web
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
