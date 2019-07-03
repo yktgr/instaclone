a7f492813e51555e27cc93467014ae5db45d4445
user <user@usernoMacBook-Pro.local>
2019-07-03 20:42:59 +0900

    user写真調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence:true, uniqueness:true
-  validates :email, presence:true, uniqueness:true,length: { minimum: 6}
+  validates :email, presence:true
   before_validation{email.downcase!}
+  validates :password, presence:true,length: { minimum: 6}
   has_secure_password
   has_many :posts
   has_many:favorites, dependent: :destroy
