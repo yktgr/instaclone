086c9b4f82a464a70eeb4caba0aef3e4edfee666
user <user@usernoMacBook-Pro.local>
2019-07-03 19:44:51 +0900

    validation追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates:title,presence:true
-  validates:content,presence:true
+  validates:title,presence:true,length:{maximum:20}
+  validates:content,presence:true,length:{maximum:140}
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many:favorites_users, through: :favorites, source: :user
